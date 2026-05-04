f8102263c66a684f0eb262cd731fcd83f5a9c293
jacob.hoeflaken <jacob.hoeflaken@axians.com>
2026-05-04 22:40:20 +0200

    First commit

--- frontend/src/features/diagram/components/Inspector.tsx
+++ frontend/src/features/diagram/components/Inspector.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {IconChevronLeft, IconChevronRight} from "@tabler/icons-react";
 
 import diagramClasses from '../diagram.module.css';
+import {Tooltip} from "@mantine/core";
 
 /**
  * The inspector properties.
@@ -17,15 +18,16 @@ interface InspectorProps {
 export const Inspector: React.FC<InspectorProps> = ({collapsed, toggle}) => {
     return (
         <div className={diagramClasses.inspectorRoot}>
+            <Tooltip label={collapsed ? 'Show inspector' : 'Hide inspector'} position="left">
             <button
                 type="button"
                 className={diagramClasses.toggleButton}
                 onClick={toggle}
                 aria-label={collapsed ? 'Show inspector' : 'Hide inspector'}
-                title={collapsed ? 'Show inspector' : 'Hide inspector'}
             >
                 {collapsed ? <IconChevronLeft size={20} /> : <IconChevronRight size={20} />}
             </button>
+            </Tooltip>
 
             {!collapsed && (
                 <div className={diagramClasses.inspectorPanel}></div>
